c16db00dfeab135c746f2575e3d1bd4082832fb7
Pavlo Pyvovar <pavlo.pyvovar@ut.ee>
2022-11-27 12:20:17 +0200

    Use image augmentatin and transformation for training

---
+++ mycar/drive.sh
@@ -0,0 +1,1 @@
+python manage.py drive --js --model models/pilot_22-10-26_1.tflite --type tflite_linear
--- mycar/myconfig.py
+++ mycar/myconfig.py
@@ -407,19 +407,19 @@ AUTO_CREATE_NEW_TUB = True
 # PRUNE_EVAL_PERCENT_OF_DATASET = .05  # percent of dataset used to perform evaluation of model.
 # 
 # # Augmentations and Transformations
-# AUGMENTATIONS = []
+AUGMENTATIONS = ["MULTIPLY", "BLUR"]
 # TRANSFORMATIONS = []
 # # Settings for brightness and blur, use 'MULTIPLY' and/or 'BLUR' in
 # # AUGMENTATIONS
-# AUG_MULTIPLY_RANGE = (0.5, 3.0)
-# AUG_BLUR_RANGE = (0.0, 3.0)
+AUG_MULTIPLY_RANGE = (0.5, 3.0)
+AUG_BLUR_RANGE = (0.0, 3.0)
 # # Region of interest cropping, requires 'CROP' in TRANSFORMATIONS to be set
 # # If these crops values are too large, they will cause the stride values to
 # # become negative and the model with not be valid.
-# ROI_CROP_TOP = 45               # the number of rows of pixels to ignore on the top of the image
-# ROI_CROP_BOTTOM = 0             # the number of rows of pixels to ignore on the bottom of the image
-# ROI_CROP_RIGHT = 0              # the number of rows of pixels to ignore on the right of the image
-# ROI_CROP_LEFT = 0               # the number of rows of pixels to ignore on the left of the image
+ROI_CROP_TOP = 50               # the number of rows of pixels to ignore on the top of the image
+ROI_CROP_BOTTOM = 0             # the number of rows of pixels to ignore on the bottom of the image
+ROI_CROP_RIGHT = 0              # the number of rows of pixels to ignore on the right of the image
+ROI_CROP_LEFT = 0               # the number of rows of pixels to ignore on the left of the image
 # # For trapezoidal see explanation in augmentations.py. Requires 'TRAPEZE' in
 # # TRANSFORMATIONS to be set
 # ROI_TRAPEZE_LL = 0
